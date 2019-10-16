3807bb1eb1a4931a3d5b9a66492764c08fded5ec
Ben Lirio <benjlirio@gmail.com>
2019-10-16 17:03:54 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 console.log("Up and Running!");
 
-cards = ["queen", "queen", "king", "king"];
+cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 cardsInPlay = [];
 
 
 
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped: " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped: " + cards[cardId].rank);
+	console.log("User flipped: " + cards[cardId].suit);
+	console.log("User flipped: " + cards[cardId].cardImage);
 	checkForMatch();
 }
 function checkForMatch() {
 	if(cardsInPlay.length === 2) {
 		if(cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
 	
