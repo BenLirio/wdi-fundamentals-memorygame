148650af7ea2d48b7d2714dfdcc23428d081f583
Ben Lirio <benjlirio@gmail.com>
2019-10-16 16:24:59 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@ console.log("Up and Running!");
 
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-cardOne = cards[0];
-cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("you found a match!");
-} else {
-	alert("sorry try again");
+
+
+
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped: " + cards[cardId]);
+	checkForMatch();
 }
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+	}
+	
+}
+flipCard(0);
+flipCard(1);
